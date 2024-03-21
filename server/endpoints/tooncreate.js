880894bd03bdc0aa1db3ff058a01183487c273b2
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2024-03-21 11:10:45 -0400

    add everything in create

--- server/endpoints/tooncreate.js
+++ server/endpoints/tooncreate.js
@@ -8,13 +8,63 @@ module.exports.verify = function (req, res) {
 }
 
 module.exports.execute = function (req, res) {
+    // we assume (perhaps too naively)
+    // that the client will always supply the correct data
     if (req.body.slug && req.body.title) {
         prisma.toon.create({
             data: {
                 slug: req.body.slug,
                 title: req.body.title,
                 alttitle: req.body.alttitle,
-                summary: req.body.summary
+                summary: req.body.summary,
+                authors: {
+                    connectOrCreate: req.body.authors.map((author) => {
+                        return {
+                            where: {
+                                name: author
+                            },
+                            create: {
+                                name: author
+                            }
+                        }
+                    })
+                },
+                artists: {
+                    connectOrCreate: req.body.artists.map((artist) => {
+                        return {
+                            where: {
+                                name: artist
+                            },
+                            create: {
+                                name: artist
+                            }
+                        }
+                    })
+                },
+                genres: {
+                    connectOrCreate: req.body.genres.map((genre) => {
+                        return {
+                            where: {
+                                name: genre
+                            },
+                            create: {
+                                name: genre
+                            }
+                        }
+                    })
+                },
+                tags: {
+                    connectOrCreate: req.body.tags.map((tag) => {
+                        return {
+                            where: {
+                                name: tag
+                            },
+                            create: {
+                                name: tag
+                            }
+                        }
+                    })
+                }
             }
         })
     }
