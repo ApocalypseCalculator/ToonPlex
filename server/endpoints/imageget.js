d15eeb84776d8e964f570f394db2e28218e46825
Patrick Lin <apocalypsecalculator@gmail.com>
2024-03-23 12:35:45 -0400

    add image fetch endpoint

---
+++ server/endpoints/imageget.js
@@ -0,0 +1,33 @@
+const { PrismaClient, UploadStatus } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports.name = "/api/image/get/:transportid";
+module.exports.method = "GET";
+module.exports.verify = function (req, res) {
+    return true;
+}
+
+module.exports.execute = function (req, res) {
+    if (req.params.transportid) {
+        prisma.image.findUnique({
+            where: {
+                transport: req.params.transportid,
+                status: UploadStatus.UPLOADED
+            }
+        }).then((image) => {
+            if(image) {
+                res.sendFile(image.path, 
+                    { root: './content', immutable: true, maxAge: 31556926 });
+                // a very long cache time
+                // if the image is updated, a new transport id is assigned
+                // to cache bust, the client must request the image with the new transport id
+            }
+            else {
+                res.status(404).json({ error: `Image not found` });
+            }
+        });
+    }
+    else {
+        res.status(400).json({ error: `Invalid form` });
+    }
+}
