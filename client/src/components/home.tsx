51f255db0668c2e885bc393316d86008dafc79c3
Patrick Lin <apocalypsecalculator@gmail.com>
2024-03-23 14:15:45 -0400

    home component

---
+++ client/src/components/home.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export const Home = () => {
+    return (
+        <>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1>ToonPlex</h1>
+                </div>
+            </div>
+            <div className="container">
+                <h4>View your favourite webtoons, on your own server</h4>
+                <p>Scrape and upload your webtoons, and view them without 
+                    the ads or slow loading times
+                </p>
+            </div>
+        </>
+    )
+}
