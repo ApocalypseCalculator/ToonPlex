5588149f11fbf2d37fc66a53ed25233ef438bb0f
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2024-03-25 21:59:22 -0400

    refactor suite style

--- scrapers/suite/index.js
+++ scrapers/suite/index.js
@@ -1,3 +1,11 @@
-const toonily = require('./toonily');
+// list all scraper modules here
 
-module.exports = [toonily]
+// every scraper object should have a name, domain and associated JS file
+// the caller will fork a scraper process and initiate the scrape
+// via IPC
+
+module.exports = [{
+    name: "toonily.com scraper",
+    domain: "toonily.com",
+    file: "toonily.js"
+}]
