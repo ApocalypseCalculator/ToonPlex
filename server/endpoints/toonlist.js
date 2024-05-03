0f01112fb4cd359d8606a680693d1bd01f7f86ff
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2024-05-03 17:20:27 -0400

    advanced query for toon list

--- server/endpoints/toonlist.js
+++ server/endpoints/toonlist.js
@@ -7,14 +7,64 @@ module.exports.verify = function (req, res) {
     return true;
 }
 
+const DEFAULT_PAGE_SIZE = 24;
+
+/*
+parameters: 
+page: number (page # for pagination)
+amount: number (amount of toons to return at given page)
+author: string (author name to filter by)
+artist: string (artist name to filter by)
+genre: string (genre name to filter by)
+tag: string (tag name to filter by)
+
+support will be added for multiple authors, artists, genres, and tags later
+
+results are ordered by id in descending order, i.e. newest to oldest
+*/
+
 module.exports.execute = function (req, res) {
-    let offset = 0;
-    if(req.query.page && !isNaN(parseInt(req.query.page)) && parseInt(req.query.page) > 0) {
-        offset = parseInt(req.query.page) * 10;
-    }
+    let amount = parseInt(req.query.amount) || DEFAULT_PAGE_SIZE;
+    let offset = (parseInt(req.query.page) || 0) * amount;
     prisma.toon.findMany({
         where: {
-            ...(!req.auth && {published: true})
+            ...((!req.auth || (!req.auth.permissions.read && !req.auth.permissions.admin)) && { published: true }),
+            ...(req.query.author && {
+                authors: {
+                    some: {
+                        name: {
+                            equals: req.query.author
+                        }
+                    }
+                }
+            }),
+            ...(req.query.artist && {
+                artists: {
+                    some: {
+                        name: {
+                            equals: req.query.artist
+                        }
+                    }
+                }
+            }),
+            ...(req.query.genre && {
+                genres: {
+                    some: {
+                        name: {
+                            equals: req.query.genre
+                        }
+                    }
+                }
+            }),
+            ...(req.query.tag && {
+                tags: {
+                    some: {
+                        name: {
+                            equals: req.query.tag
+                        }
+                    }
+                }
+            })
         },
         select: {
             id: true,
@@ -35,9 +85,12 @@ module.exports.execute = function (req, res) {
                 }
             }
         },
-        take: 10,
-        skip: offset
+        take: amount,
+        skip: offset,
+        orderBy: {
+            id: 'desc'
+        }
     }).then((toons) => {
-        res.json({ status: 200, offset: offset, toons: toons });
+        res.json({ status: 200, offset: offset, amount: amount, toons: toons });
     });
 }
