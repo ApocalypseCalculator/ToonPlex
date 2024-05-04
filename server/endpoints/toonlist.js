a17516d2004447b7901fa066a5dee159ec3db953
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2024-05-04 01:14:22 -0400

    return total count in toonlist

--- server/endpoints/toonlist.js
+++ server/endpoints/toonlist.js
@@ -26,71 +26,77 @@ results are ordered by id in descending order, i.e. newest to oldest
 module.exports.execute = function (req, res) {
     let amount = parseInt(req.query.amount) || DEFAULT_PAGE_SIZE;
     let offset = (parseInt(req.query.page) || 0) * amount;
-    prisma.toon.findMany({
-        where: {
-            ...((!req.auth || (!req.auth.permissions.read && !req.auth.permissions.admin)) && { published: true }),
-            ...(req.query.author && {
-                authors: {
-                    some: {
-                        name: {
-                            equals: req.query.author
-                        }
+    let queryfilter = {
+        ...((!req.auth || (!req.auth.permissions.read && !req.auth.permissions.admin)) && { published: true }),
+        ...(req.query.author && {
+            authors: {
+                some: {
+                    name: {
+                        equals: req.query.author
                     }
-                }
-            }),
-            ...(req.query.artist && {
-                artists: {
-                    some: {
-                        name: {
-                            equals: req.query.artist
-                        }
-                    }
-                }
-            }),
-            ...(req.query.genre && {
-                genres: {
-                    some: {
-                        name: {
-                            equals: req.query.genre
-                        }
-                    }
-                }
-            }),
-            ...(req.query.tag && {
-                tags: {
-                    some: {
-                        name: {
-                            equals: req.query.tag
-                        }
-                    }
-                }
-            })
-        },
-        select: {
-            id: true,
-            slug: true,
-            title: true,
-            authors: true,
-            artists: true,
-            genres: true,
-            tags: true,
-            status: true,
-            published: true,
-            cover: {
-                where: {
-                    status: UploadStatus.UPLOADED
-                },
-                select: {
-                    transport: true
                 }
             }
-        },
-        take: amount,
-        skip: offset,
-        orderBy: {
-            id: 'desc'
-        }
-    }).then((toons) => {
-        res.json({ status: 200, offset: offset, amount: amount, toons: toons });
-    });
+        }),
+        ...(req.query.artist && {
+            artists: {
+                some: {
+                    name: {
+                        equals: req.query.artist
+                    }
+                }
+            }
+        }),
+        ...(req.query.genre && {
+            genres: {
+                some: {
+                    name: {
+                        equals: req.query.genre
+                    }
+                }
+            }
+        }),
+        ...(req.query.tag && {
+            tags: {
+                some: {
+                    name: {
+                        equals: req.query.tag
+                    }
+                }
+            }
+        })
+    };
+    Promise.all([
+        prisma.toon.count({
+            where: queryfilter
+        }),
+        prisma.toon.findMany({
+            where: queryfilter,
+            select: {
+                id: true,
+                slug: true,
+                title: true,
+                authors: true,
+                artists: true,
+                genres: true,
+                tags: true,
+                status: true,
+                published: true,
+                cover: {
+                    where: {
+                        status: UploadStatus.UPLOADED
+                    },
+                    select: {
+                        transport: true
+                    }
+                }
+            },
+            take: amount,
+            skip: offset,
+            orderBy: {
+                id: 'desc'
+            }
+        })
+    ]).then(([count, toons]) => {
+        res.json({ status: 200, offset: offset, amount: amount, total: count, toons: toons });
+    })
 }
