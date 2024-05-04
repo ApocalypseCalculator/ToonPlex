b53fc0b41225699dd0a2bff9ae00bfae491abc0e
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2024-05-03 21:23:25 -0400

    consolidate into 1 context state

--- client/src/util/session.tsx
+++ client/src/util/session.tsx
@@ -1,29 +1,46 @@
 import * as React from 'react';
 
-export interface Session {
+export interface SessionData {
+    username: string,
     token: string,
+    ready: boolean
+}
+export interface Session {
+    data: SessionData,
     updateToken: (token: string) => void
 }
 
-export const SessionContext = React.createContext<Session>({
-    token: "",
-    updateToken: (token: string) => { }
-});
+export const SessionContext = React.createContext<Session>({} as any);
 
 export const SessionProvider = (props: { children: React.ReactNode }) => {
-    let [token, setToken] = React.useState("");
+    let [data, setData] = React.useState({
+        username: "",
+        token: "",
+        ready: false
+    } as SessionData);
     function updateToken(token: string) {
-        setToken(token);
+        setData({
+            username: "",
+            token: token,
+            ready: true
+        });
         localStorage.setItem("token", token);
     }
     React.useEffect(() => {
         let storagetoken = localStorage.getItem("token");
-        if (storagetoken) {
+        if (storagetoken && storagetoken !== "") {
             updateToken(storagetoken);
+        }
+        else {
+            setData({
+                username: "",
+                token: "",
+                ready: true
+            });
         }
     }, []);
     return (
-        <SessionContext.Provider value={{ token, updateToken }}>
+        <SessionContext.Provider value={{ data, updateToken}}>
             {props.children}
         </SessionContext.Provider>
     )
