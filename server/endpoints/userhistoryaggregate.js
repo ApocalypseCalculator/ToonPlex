301a88d74d8514b6fb97f83a1e4e8d78adbf69f3
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2024-05-13 00:52:16 -0400

    paginate read history

--- server/endpoints/userhistoryaggregate.js
+++ server/endpoints/userhistoryaggregate.js
@@ -7,18 +7,34 @@ module.exports.verify = function (req, res) {
     return req.auth;
 }
 
+const DEFAULT_PAGE_SIZE = 6;
+
 module.exports.execute = function (req, res) {
+    let amount = parseInt(req.query.amount) || DEFAULT_PAGE_SIZE;
+    let page = (parseInt(req.query.page) || 1);
+    let offset = (page - 1) * amount;
     // This raw SQL query (targetted specifically for Postgres)
     // retrieves the highest ordered chapter for each toon that the user has read
-    prisma.$queryRaw`SELECT date, chapterid, "order", "Toon".id as toonid, "Toon".slug as toonslug, "Toon".title as toontitle FROM 
+    prisma.$queryRaw`SELECT date, chapterid, "order", "Toon".id as toonid, "Toon".slug as toonslug, "Toon".title as toontitle, "Image".transport FROM 
     (SELECT DISTINCT ON ("Chapter".toonid) "ReadHistory".date, "Chapter".id as chapterid, "Chapter".order, "Chapter".toonid  
     FROM "ReadHistory" 
     INNER JOIN "Chapter" ON "ReadHistory".chapterid = "Chapter".id 
     WHERE userid=${req.auth.id} 
     ORDER BY "Chapter".toonid ASC, "Chapter".order DESC) 
-    INNER JOIN "Toon" ON toonid = "Toon".id;`.then((history) => {
+    INNER JOIN "Toon" ON toonid = "Toon".id 
+    LEFT JOIN "Image" ON "Toon".coverid = "Image".id 
+    ORDER BY "date" DESC 
+    OFFSET ${offset} LIMIT ${amount == 0 ? 'NULL' : amount};`.then((history) => {
+        history.map((toon) => {
+            toon.cover = {
+                transport: toon.transport
+            };
+            delete toon.transport;
+            return toon;
+        });
         res.json({ status: 200, history: history });
     }).catch(err => {
+        console.error(err);
         res.status(500).json({ error: `Internal server error` });
     })
 }
